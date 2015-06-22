cb767761eb608bcd41e5c440941ec68c2e368d8e
Michael Todd <mikejtodd@gmail.com>
2015-06-21 22:09:52 -0500

    Email_processor fix?

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,12 +1,14 @@
-def initialize(email)
-  @email = email
-  binding.remote_pry
-end
+class EmailProcessor
+  def initialize(email)
+    @email = email
+    binding.remote_pry
+  end
 
-def process
-  binding.remote_pry
-  Post.create!(
-    headline: @email.subject,
-    body: @email.body
-  )
+  def process
+    binding.remote_pry
+    Post.create!(
+      headline: @email.subject,
+      body: @email.body
+    )
+  end
 end
