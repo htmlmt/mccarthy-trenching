eb67691c645f117fd9a791f70a0ded577e6bc264
Michael Todd <mikejtodd@gmail.com>
2015-06-21 21:33:45 -0500

    Added griddler route back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
+  post '/email_processor' => 'griddler/emails#create'
   resources :concerts
   resources :posts
 end
