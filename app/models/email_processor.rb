3d2fe49b69054b06bdb0097a2bba69b221f75c43
Michael Todd <mikejtodd@gmail.com>
2015-06-21 22:27:20 -0500

    Model for email_processor

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,6 +1,16 @@
 class EmailProcessor
-  def self.process(email)
-    @post = Post.new(body: email.body, headline: email.subject)
-    @post.save
+  def initialize(email)
+    @email = email
+  end
+
+  def process
+    # all of your application-specific code here - creating models,
+    # processing reports, etc
+
+    # here's an example of model creation
+    Post.create!(
+      headline: @email.subject,
+      body: @email.text
+    )
   end
 end
