225d5e4448ca368576ada05f6b2438d7893adf85
Michael Todd <mikejtodd@gmail.com>
2015-06-23 12:40:32 -0500

    Removed to address Post.create

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,27 +4,25 @@ class EmailProcessor
   end
 
   def process
-    Post.create!(
-      headline: @email.to[0][:email]
-    )
-    if @email.to[0][:email] == "lastnews@mctrenching.com"
+    to_address = @email.to[0][:email]
+    if to_address == "lastnews@mctrenching.com"
       @post = Post.last
       @post.update(
         headline: @email.subject,
         body: @email.body
       )
-    elsif @email.to[0][:email] == "lastshow@mctrenching.com"
+    elsif to_address == "lastshow@mctrenching.com"
       @show = Show.last
       @show.update(
         date: DateTime.parse(@email.subject),
         description: @email.raw_body
       )
-    elsif @email.to[0][:email] == "news@mctrenching.com"
+    elsif to_address == "news@mctrenching.com"
       Post.create!(
         headline: @email.subject,
         body: @email.body
       )
-    elsif @email.to[0][:email] == "shows@mctrenching.com"
+    elsif to_address == "shows@mctrenching.com"
       Show.create!(
         date: DateTime.parse(@email.subject),
         description: @email.raw_body
