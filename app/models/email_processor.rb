1d55bcc5f1222f1f25aa05dc25fdfbd0b0c0b056
Michael Todd <mikejtodd@gmail.com>
2015-06-22 22:50:39 -0500

    Added shows, last show and last news for email post

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,17 +4,30 @@ class EmailProcessor
   end
 
   def process
-    if @email.to[:email] == "editlast@mctrenching.com"
+    if @email.to[:email] == "lastnews@mctrenching.com"
       @post = Post.last
       @post.update(
         headline: @email.subject,
         body: @email.body
       )
-    else
+    elsif @email.to[:email] == "lastshow@mctrenching.com"
+      @show = Show.last
+      @show.update(
+        date: DateTime.parse(@email.subject),
+        description: @email.raw_body
+      )
+    elsif @email.to[:email] == "news@mctrenching.com"
       Post.create!(
         headline: @email.subject,
         body: @email.body
       )
+    elsif @email.to[:email] == "shows@mctrenching.com"
+      Show.create!(
+        date: DateTime.parse(@email.subject),
+        description: @email.raw_body
+      )
+    else
+      
     end
   end
 end
