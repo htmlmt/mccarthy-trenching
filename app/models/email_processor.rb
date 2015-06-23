a4f4169cd50c3e03f8caf1500e8e2798d3b0f0c8
Michael Todd <mikejtodd@gmail.com>
2015-06-22 23:08:44 -0500

    Working on Symbol to Integer issue

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,28 +1,27 @@
 class EmailProcessor
   def initialize(email)
-    @email = {}
     @email = email
   end
 
   def process
-    if @email.to[:email] == "lastnews@mctrenching.com"
+    if @email.to == "lastnews@mctrenching.com"
       @post = Post.last
       @post.update(
         headline: @email.subject,
         body: @email.body
       )
-    elsif @email.to[:email] == "lastshow@mctrenching.com"
+    elsif @email.to == "lastshow@mctrenching.com"
       @show = Show.last
       @show.update(
         date: DateTime.parse(@email.subject),
         description: @email.raw_body
       )
-    elsif @email.to[:email] == "news@mctrenching.com"
+    elsif @email.to == "news@mctrenching.com"
       Post.create!(
         headline: @email.subject,
         body: @email.body
       )
-    elsif @email.to[:email] == "shows@mctrenching.com"
+    elsif @email.to == "shows@mctrenching.com"
       Show.create!(
         date: DateTime.parse(@email.subject),
         description: @email.raw_body
