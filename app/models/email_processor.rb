e5dffd97385f98cb8b65d790790c7f2c4701838d
Michael Todd <mikejtodd@gmail.com>
2015-06-24 20:37:10 -0500

    Saving raw_html

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,27 +5,28 @@ class EmailProcessor
 
   def process
     to_address = @email.to[0][:email]
+    content = @email.raw_html
     if to_address == "lastnews@mctrenching.com"
       @post = Post.last
       @post.update(
         headline: @email.subject,
-        body: @email.raw_text
+        body: content
       )
     elsif to_address == "lastshow@mctrenching.com"
       @show = Show.last
       @show.update(
         date: DateTime.parse(@email.subject),
-        description: @email.raw_text
+        description: content
       )
     elsif to_address == "news@mctrenching.com"
       Post.create!(
         headline: @email.subject,
-        body: @email.raw_text
+        body: content
       )
     elsif to_address == "shows@mctrenching.com"
       Show.create!(
         date: DateTime.parse(@email.subject),
-        description: @email.raw_text
+        description: content
       )
     else
       
